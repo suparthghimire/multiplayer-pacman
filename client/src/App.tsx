86090ab2adb5402b2e60c6f2481c672f4028b821
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-12-16 13:49:33 +0545

    p5 Initialization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import Sketch from "react-p5";
+import p5Types from "p5";
 function App() {
-  return <div>Hey Guy</div>;
+  function setup(p5: p5Types, canvasParentRef: Element) {
+    p5.createCanvas(700, 700).parent(canvasParentRef);
+    p5.background("black");
+  }
+  function draw(p5: p5Types) {
+    p5.background("black");
+    p5.fill(255);
+    p5.rect(0, 0, 100, 100);
+  }
+
+  return <Sketch draw={draw} setup={setup} />;
 }
 
 export default App;
