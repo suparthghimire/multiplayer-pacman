1d6376373f07ef5245059824e6eb86367a90b902
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-12-16 13:42:43 +0545

    Start From Scratch

--- client/src/model/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-export default class Helper {
-  public static RandomRange(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-}
--- client/src/model/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export enum MAP_CELL {
-  EMPTY = 0,
-  WALL = 1,
-  PACMAN = 2,
-  GHOST = 3,
-  FOOD = 4,
-  ORB = 5,
-}
